59ab00d82d122f767e7c7b6885131172853ce118
shahareldad <shahareldad@gmx.com>
2015-05-24 22:40:49 +0300

    First check-in

---
+++ WPF_MVVM/ActionCommand.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WPF_MVVM
+{
+    public class ActionCommand : ICommand
+    {
+        private readonly Func<bool> _canExecute;
+        private readonly Action _action;
+
+        public ActionCommand(Action action)
+            : this(action, null)
+        {
+        }
+
+        public ActionCommand(Action action, Func<bool> canExecute)
+        {
+            _canExecute = canExecute;
+            _action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute != null)
+                return _canExecute();
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+
+    public class ActionCommand<T> : ICommand
+    {
+        private readonly Func<bool> _canExecute;
+        private readonly Action<T> _action;
+
+        public ActionCommand(Action<T> action)
+            : this(action, null)
+        {
+        }
+
+        public ActionCommand(Action<T> action, Func<bool> canExecute)
+        {
+            _canExecute = canExecute;
+            _action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute != null)
+                return _canExecute();
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
---
+++ WPF_MVVM/PropertyChangedBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace WPF_MVVM
+{
+    public class PropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(Expression<Func<object>> expression)
+        {
+            var memberExpression = expression.Body as MemberExpression;
+            if (memberExpression != null)
+                OnPropertyChanged(memberExpression.Member.Name);
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
